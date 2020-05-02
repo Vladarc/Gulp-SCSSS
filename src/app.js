fec927e1a40f73aed752ac03b4514f580e2b6c1f
Vladarc <vladmironecdonnu@gmail.com>
2020-05-02 23:44:41 +0300

    add menu

--- src/app.js
+++ src/app.js
@@ -6,3 +6,20 @@ intlTelInput(input, {
  
 }); 
 
+const toggler = (event) =>{
+    event.preventDefault()
+    document.body.classList.toggle('opened')
+}
+
+window.onresize = displayWindowSize;
+window.onload = displayWindowSize;
+
+function displayWindowSize() {
+    return  window.innerWidth;
+  
+};
+if(displayWindowSize <= 992){
+    console.log(1)
+}else{
+    console.log(2)
+}
