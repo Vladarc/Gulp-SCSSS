2e1cbdb5811c19cec8f20c070cc85c45f7d19cc7
Vladarc <vladmironecdonnu@gmail.com>
2020-05-03 01:27:22 +0300

    add scroll top btn

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-let input = document.querySelector("#user_phone");
+let input = document.querySelector("#user_phone"); // plugin for check mobile nubmer 
 intlTelInput(input, {
     utilsScript:'./src/utils.js',
     preferredCountries:["ua"],
@@ -6,20 +6,51 @@ intlTelInput(input, {
  
 }); 
 
+const BODY = document.body
+
+
+// toggle burger menu 
 const toggler = (event) =>{
     event.preventDefault()
-    document.body.classList.toggle('opened')
+    BODY.classList.toggle('opened')
 }
 
-window.onresize = displayWindowSize;
-window.onload = displayWindowSize;
+//scroll to top
+let scrollToTopBtn = document.querySelector('.back-to-top')
 
-function displayWindowSize() {
-    return  window.innerWidth;
-  
-};
-if(displayWindowSize <= 992){
-    console.log(1)
-}else{
-    console.log(2)
+const scrollFunction = () =>{
+
+    if(window.pageYOffset > 400){
+        scrollToTopBtn.classList.add('back-to-top--active')
+    }else{
+        scrollToTopBtn.classList.remove('back-to-top--active')
+    }
 }
+
+const scrollToTop = (event) => {
+    window.scrollTo(0,0)
+}
+
+
+//mobile menu 
+const headerMenu = document.querySelector('.nav__list')
+
+const addEvent =  () => {
+    if(window.innerWidth <= 992){
+        console.log("addEvent")
+        headerMenu.addEventListener('click',(event) => {
+            let {target} = event 
+            let link = target.classList.contains('nav__link')
+                if(link){
+                    BODY.classList.remove('opened')
+                   
+                }
+        })
+
+    }
+}
+
+scrollToTopBtn.addEventListener('click',scrollToTop)
+window.addEventListener('scroll',scrollFunction)
+window.addEventListener('resize', addEvent)
+addEvent()
